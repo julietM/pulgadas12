e74c9003ca869d68a98f03eab6f70010f7129355
juliet@21 <julietyartes2@gmail.com>
2022-04-05 09:25:55 -0500

    termina

---
+++ script.js
@@ -0,0 +1,10 @@
+let pulgadas=0.0254;
+let conversion;
+
+
+let numberPulgada = Number(prompt('ingresa cantidad de pulgada'));
+
+conversion=numberPulgada*pulgadas;
+
+document.write(conversion,"metros");
+
